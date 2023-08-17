45cf6fadd659cfb9b8fed56ff9a04b6437892875
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-17 10:19:44 +0100

    Fix : Refactor query

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,14 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Products.find(req.query);
+  const { featured } = req.query;
+  const queryOBject = {};
+
+  if (featured) {
+    queryOBject.featured = featured === "true" ? true : false;
+  }
+  console.log(queryOBject);
+  const products = await Products.find(queryOBject, null, { strict: false });
   res.status(200).json({ nbHits: products.length, products });
 };
 
