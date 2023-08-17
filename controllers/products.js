3d6f0d26bacbd8c276ff4bf9d4903c7ce9cfdb50
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-17 09:11:12 +0100

    Feature- Basic find

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,9 @@
+const Products = require("../models/Products");
+// const Product = require("../models/Products");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "Product Testing" });
+  const products = await Products.find({ featured: true });
+  res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
