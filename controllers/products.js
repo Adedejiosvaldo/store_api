9421f203c1178ce277fa77dd3d97d44e4ee0ccd1
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-18 09:16:26 +0100

    Featured : Sort Functionality

--- controllers/products.js
+++ controllers/products.js
@@ -2,12 +2,12 @@ const Products = require("../models/Products");
 // const Product = require("../models/Products");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Products.find({ featured: true });
+  const products = await Products.find({}).sort("name price");
   res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryOBject = {};
 
   if (featured) {
@@ -19,8 +19,20 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryOBject.name = { $regex: name, $options: "i" };
   }
-  console.log(queryOBject);
-  const products = await Products.find(queryOBject, null, { strict: false });
+
+  let result = Products.find(queryOBject, null, {
+    strict: false,
+  });
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    console.log(sortList);
+
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
 
