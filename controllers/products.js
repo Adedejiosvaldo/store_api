be1009e48e03dd595a2b422be226ad1725534564
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-19 08:28:19 +0100

    Added Numeric Sorting

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, select } = req.query;
+  const { featured, company, name, sort, select, numericFilters } = req.query;
   const queryOBject = {};
 
   if (featured) {
@@ -18,6 +18,33 @@ const getAllProducts = async (req, res) => {
   }
   if (name) {
     queryOBject.name = { $regex: name, $options: "i" };
+  }
+
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+
+    const regEx = /\b(<|>|>=|<=|=)\b/g;
+
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [select, operator, value] = item.split("-");
+      if (options.includes(select)) {
+        queryOBject[select] = { [operator]: Number(value) };
+      }
+    });
+    console.log(filters);
+    console.log(queryOBject);
   }
 
   let result = Products.find(queryOBject, null, {
